f3dd2c0591ab113bd51e756001d556da90434f4e
Ana Almeida <ana.almeida@ab-inbev.com>
2022-06-02 23:16:40 -0300

    adding env variables

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,14 +1,14 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA4AG4jJ-KmzPJADMt75RxP4irGK9ZVzzo",
-  authDomain: "sofistiquee-brigaderia.firebaseapp.com",
-  databaseURL: "https://sofistiquee-brigaderia-default-rtdb.firebaseio.com",
-  projectId: "sofistiquee-brigaderia",
-  storageBucket: "sofistiquee-brigaderia.appspot.com",
-  messagingSenderId: "1006903018440",
-  appId: "1:1006903018440:web:84cab9101374291bd776fd",
-  measurementId: "G-DFRY4SYFHF",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_BASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 export const app = initializeApp(firebaseConfig);
